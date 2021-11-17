2ac489ce0d6b9d22c48de02c4621f276804313ae
Smaug-The-Golden <89695506+Smaug-The-Golden@users.noreply.github.com>
2021-11-17 23:03:39 +0300

    Add files via upload

---
+++ Task_1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+
+namespace Final_task_8_1
+{
+    class Task_1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            double erase_time = TimeSelect();
+            string dir_path = DirSelect();
+            var duration = DateTime.Now - Directory.GetLastAccessTime(dir_path);
+            var duration2 = TimeSpan.FromMinutes(erase_time);
+            int check = TimeSpan.Compare(duration, duration2);
+
+            Console.WriteLine($"Значение переменной duration: {duration}");
+            Console.WriteLine($"Значение переменной duration2: {duration2}");
+            Console.WriteLine($"Значение переменной check: {check}");
+
+            if (check >= 1)
+            {
+                try
+                {
+                    Directory.Delete(dir_path, true);
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("\nУдаление каталога успешно завершено!");
+                    Console.ResetColor();
+                    Console.ReadLine();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Возникла некоторая исключительная ситуация. Выполнение программы прервано!");
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nУдаление каталога не возможно! Прошло недостаточно времени для удаления ");
+                Console.ResetColor();
+                Console.ReadLine();
+            }
+
+        }
+
+        static double TimeSelect()
+        {
+            Console.WriteLine("\t\nУкажите время в минутах, по истечению которого будет произведена очистка каталога: ");
+            double erase_time;
+            do
+            {
+                if (double.TryParse(Console.ReadLine(), out erase_time) && erase_time >= 1 && erase_time <= double.MaxValue)
+                {
+                    break;
+                }
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nВы ввели некорректное значение!\nПожалуйста, введите целое положительное число: ");
+                Console.ResetColor();
+            }
+            while (true);
+            return (erase_time);
+        }
+
+        static string DirSelect()
+        {
+            Console.WriteLine("\nУкажите путь до каталога: ");
+            string dir_path = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            if (Directory.Exists(dir_path))
+                Console.WriteLine($"\nКаталог существует, он был создан: {Directory.GetCreationTime(dir_path)} \n" +
+                                    $"Время последней записи в данный каталог: {Directory.GetLastWriteTime(dir_path)} \n" +
+                                    $"Время последнего обращения к данному каталогу: {Directory.GetLastAccessTime(dir_path)}");
+            else
+            {
+                do
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nКаталога по данному адресу не существует!\nПожалуйста, укажите путь до каталога еще раз: ");
+                    Console.ResetColor();
+                    dir_path = Console.ReadLine();
+                }
+                while (!Directory.Exists(dir_path));
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"\nКаталог существует, он был создан: {Directory.GetCreationTime(dir_path)} \n" +
+                                    $"Время последней записи в данный каталог: {Directory.GetLastWriteTime(dir_path)} \n" +
+                                    $"Время последнего обращения к данному каталогу: {Directory.GetLastAccessTime(dir_path)}");
+            }
+            Console.ResetColor();
+            return (dir_path);
+        }
+    }
+}
